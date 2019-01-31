b88107260a5ab2d16c7923c5a0788aea3e2abd11
Kiran Gopal <kirgopal0@publicisgroupe.net>
2019-01-31 20:55:52 +0530

    firstcommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductDetailsComponent,
+    NotFoundPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
